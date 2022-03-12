5023701333f1fab313e28b9ab15bb3211ab1ab26
Zuzanna Debska <zuzanna.debska4321@gmail.com>
2022-03-12 16:31:02 +0100

    Global dictionaries for request counts per user ID

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
 import sqlite3, json
+from multiprocessing import Manager
 
 app = Flask(__name__)
+
+def create_hourly_stats():
+    print(total_request_count)
+    print(invalid_request_count)
+
 
 # Empty stub function for valid requests
 def process_request(customerID, tagID, userID, remoteIP, timestamp):
@@ -46,20 +52,18 @@ def checkValidJson(data):
 @app.route('/', methods = ['POST'])
 def index():
 
+    try:
+        request_data = request.get_json()
+        customer_id = request_data['customerID']
+        total_request_count[customer_id] += 1
+    except:
+        print("Can't count request: can't find customer id!")
+
     # Message to return on POST Endpoint (will be replaced depending on validity of request)
     returnString = 'Invalid request'
 
     # returns true or false
     if checkValidJson(request.data):
-
-    # Sample JSON
-    # {
-    #     "customerID": 1,
-    #     "tagID": 2,
-    #     "userID": "aaaaaaaa-bbbb-cccc-1111-222222222222",
-    #     "remoteIP": "123.234.56.78",
-    #     "timestamp": 1500000000
-    # }
 
         customerID, tagID, userID, remoteIP, timestamp = checkValidJson(request.data)
 
@@ -122,4 +126,24 @@ def index():
 
 
 if __name__ == '__main__':
+
+    '''Create global dictionaries storing total and invalid request counts for every user'''
+
+    # Save customer IDs from database to a list
+    connection = sqlite3.connect('database.db')
+    cur = connection.cursor()
+
+    customer_ids_result = cur.execute('SELECT id FROM customer').fetchall()
+    customer_ids = list(map(lambda x: x[0], customer_ids_result))
+
+    connection.close()
+
+    # Start total and invalid request count dictionaries to a manager to keep track of requests
+    manager = Manager()
+    total_request_count = manager.dict({i: 0 for i in customer_ids})
+    invalid_request_count = manager.dict({i: 0 for i in customer_ids})
+
+
+    '''Start app'''
+
     app.run(debug = True, port=5000)
