affe77727483a2504be73c1c421abe5e9f8f15fc
Zuzanna Debska <zuzanna.debska4321@gmail.com>
2022-03-12 12:05:22 +0100

    Check if valid JSON

--- app.py
+++ app.py
@@ -3,15 +3,54 @@ import sqlite3, json
 
 app = Flask(__name__)
 
-invalid_request_count = 0
-total_request_count = 0
+# Empty stub function for valid requests
+def process_request(customerID, tagID, userID, remoteIP, timestamp):
+    print('Valid request')
+    # pass
+
+
+def checkValidJson(data):
+    try:
+        json_data = json.loads(data)
+        customerID, tagID, userID, remoteIP, timestamp = (json_data['customerID'], json_data['tagID'], json_data['userID'], json_data['remoteIP'], json_data['timestamp'])
+
+        if not isinstance(customerID, int):
+            print('customerID must be a number!')
+            return False
+        
+        if not isinstance(tagID, int):
+            print('tagID must be a number!')
+            return False
+        
+        if not isinstance(userID, str):
+            print('userID must be a string!')
+            return False
+        
+        if not isinstance(remoteIP, str):
+            print('remoteIP must be a string!')
+            return False
+
+        if not isinstance(timestamp, int):
+            print('timestamp must be a number!')
+            return False
+
+        return customerID, tagID, userID, remoteIP, timestamp
+
+    except (ValueError, KeyError, TypeError):
+        
+        print('Invalid JSON!')
+        return False
+
 
 
 @app.route('/', methods = ['POST'])
 def index():
-    connection = sqlite3.connect('database.db')
 
-    total_request_count  = total_request_count + 1
+    # Message to return on POST Endpoint (will be replaced depending on validity of request)
+    returnString = 'Invalid request'
+
+    # returns true or false
+    if checkValidJson(request.data):
 
     # Sample JSON
     # {
@@ -22,23 +61,63 @@ def index():
     #     "timestamp": 1500000000
     # }
 
-    try:
-        data = json.loads(request.data)
-        customerID, tagID, userID, remoteIP, timestamp = (data['customerID'], data['tagID'], data['userID'], data['remoteIP'], data['timestamp'])
-    except (ValueError, KeyError, TypeError):
+        customerID, tagID, userID, remoteIP, timestamp = checkValidJson(request.data)
 
-        invalid_request_count = invalid_request_count + 1
+        returnString = 'customerID: ' + str(customerID) + '\n tagID: ' +  str(tagID) + '\n userID: ' +  str(userID) + '\n remoteIP: ' +  str(remoteIP) + '\n timestamp: ' +  str(timestamp)
 
-        return 'Invalid request!'
+        # Open database
+        connection = sqlite3.connect('database.db')
+
+        cur = connection.cursor()
+
+        # Check IP
+        try:
+            ip = remoteIP.replace('.', '')
+            ip = int(ip)
+        except:
+            returnString = 'Incorrect IP!'
+            # TODO: exit and return invlaid request
+
+
+        # Check if IP is blacklisted
+        ip_blacklist = cur.execute('SELECT * FROM ip_blacklist').fetchall()
+
+        for ip_bl in ip_blacklist:
+            if ip_bl[0] == ip:
+                returnString = 'Blacklisted IP!'
+                # TODO: exit and return invlaid request
+
+
+        # Check if username contains blacklisted user agent
+        ua_blacklist = cur.execute('SELECT * FROM ua_blacklist').fetchall()     
+
+        for ua in ua_blacklist:
+            if ua[0] in userID:
+                returnString = 'Blacklisted User Agent!'
+                # TODO: exit and return invlaid request
+
+
+        # Check if customer exists and is active
+        try:
+            customerActivity = cur.execute('SELECT active FROM customer WHERE id = ?', (customerID,)).fetchall()
+
+            if customerActivity[0][0] == 0:
+                returnString = 'Customer inactive!'
+                # TODO: exit and return invlaid request
+
+        except:
+            returnString = 'Customer not in the database!'
+            # TODO: exit and return invlaid request
+
+        connection.close()
+
+
+        process_request(customerID, tagID, userID, remoteIP, timestamp)
 
 
 
-    cur = connection.cursor()
-    customers = cur.execute('SELECT * FROM customer').fetchall()
+    return returnString
 
-    connection.close()
-
-    return str(customerID) + str(tagID) + str(userID) + str(remoteIP) + str(timestamp)
 
 
 
