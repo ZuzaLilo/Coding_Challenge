e52d63a6b0a6f15f808c692e1965910092549325
Zuzanna Debska <zuzanna.debska4321@gmail.com>
2022-03-13 20:50:09 +0100

    Comments and formatting

--- app.py
+++ app.py
@@ -12,27 +12,24 @@ from dateutil import tz
 app = Flask(__name__)
 
 
-# Empty stub function for valid requests
+# --- Empty stub function for valid requests --- 
 def process_request(customerID, tagID, userID, remoteIP, timestamp):
-    returnString = 'customerID: ' + str(customerID) + '\n tagID: ' +  str(tagID) + '\n userID: ' +  str(userID) + '\n remoteIP: ' +  str(remoteIP) + '\n timestamp: ' +  str(timestamp)
-
-    print('Valid request: ' + returnString)
-    # pass
+    pass
 
 
+# --- Create hourly stats ---
+# 
+# Insert to database values stored in:
+# - total_request_count
+# - invalid_request_count
 def create_hourly_stats():
-    print("\n")
-    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
+    # Get timestamp for current time
     time_now = time.time()
 
+    # Connect to database
     connection = sqlite3.connect('database.db')
     cur = connection.cursor()
-
-    print("----total_request_count-----")
-    print(total_request_count)
-    print("----invalid_request_count-----")
-    print(invalid_request_count)
 
     for client_id, req_count in total_request_count.items():
         cur.execute('INSERT INTO hourly_stats (customer_id, time, request_count, invalid_count) VALUES (?, ?, ?, ?)', (client_id, time_now, req_count, invalid_request_count[client_id]))
@@ -41,45 +38,65 @@ def create_hourly_stats():
         total_request_count[client_id] = 0
         invalid_request_count[client_id] = 0
 
+    # Close connection to database
     connection.commit()
     connection.close()
 
 
+# --- Check if received valid JSON ---
+# 
+# Returns customerID, tagID, userID, remoteIP, timestamp  
+# or
+# Returns False (if JSON is not valid)
 def checkValidJson(data):
     try:
         json_data = json.loads(data)
         customerID, tagID, userID, remoteIP, timestamp = (json_data['customerID'], json_data['tagID'], json_data['userID'], json_data['remoteIP'], json_data['timestamp'])
-
+        
+        # Check datatypes
         if not (isinstance(customerID, int) and isinstance(tagID, int) and isinstance(userID, str) and isinstance(remoteIP, str) and isinstance(timestamp, int)):
-            print('Wrong data type!')
             return False
         else:
+            # If successfull and no errors then json is valid and pass extracted values
             return customerID, tagID, userID, remoteIP, timestamp
 
     except (ValueError, KeyError, TypeError):
-        
-        print('Invalid JSON!')
         return False
 
-# On first request start scheduler for hourly statistics
+# --- Before first request ---
+# 
+# Start scheduler for hourly statistics
 @app.before_first_request
 def init_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=create_hourly_stats, trigger="interval", seconds=60)
+    scheduler.add_job(func=create_hourly_stats, trigger="interval", minutes=60)
     scheduler.start()
 
     # Shut down the scheduler when exiting the app
     atexit.register(lambda: scheduler.shutdown())
 
 
+# --- POST json data to this '/' endpoint ---
+# 
+# Example json:
+# { 
+#   "customerID":1,
+#   "tagID":2,
+#   "userID":"aaaaaaaa-bbbb-cccc-1111-222222222222",
+#   "remoteIP":"123.234.56.78",
+#   "timestamp":1500000000
+# }
 @app.route('/', methods = ['POST'])
 def index():
     
+    # First check if user provided "authentication" credentials
     try:
-        #  To send request user must provide their credentials first as a parameter
+        #  Get customer_id from header parameters
         header_customer_id = int(request.headers.get('customer_id'))
-        
+
+        # Check if customer_id has been sourced from database
         try:
+            # If yes, then increase total request count
             total_request_count[header_customer_id] += 1
         except:
             return "Customer not in the database!"
@@ -88,11 +105,12 @@ def index():
         return "To access this service please provide your credentials (customer_id) as a parameter!"
 
 
-    # returns true or false
     if checkValidJson(request.data):
 
+        # Get data if valid JSON
         customerID, tagID, userID, remoteIP, timestamp = checkValidJson(request.data)
 
+        # Check if customerID in JSON matches customer_id provided in credentials
         if int(customerID) != header_customer_id:
             invalid_request_count[header_customer_id] += 1
             return "You can only make a request with customer_id that matches provided credentials"
@@ -101,22 +119,19 @@ def index():
         connection = sqlite3.connect('database.db')
         cur = connection.cursor()
 
-        # Check if customer exists and is active
-        try:
-            customerActivity = cur.execute('SELECT active FROM customer WHERE id = ?', (customerID,)).fetchall()
-
-            if customerActivity[0][0] == 0:
-                invalid_request_count[header_customer_id] += 1
-                return 'Customer inactive!'
-
-        except:
-            invalid_request_count[header_customer_id] += 1
-            return 'Given customer_id not in the database!'
-
+        # Get blacklists from database
         ip_blacklist = cur.execute('SELECT * FROM ip_blacklist').fetchall()
         ua_blacklist = cur.execute('SELECT * FROM ua_blacklist').fetchall()
+        
+        # Get customer activity from database
+        customerActivity = cur.execute('SELECT active FROM customer WHERE id = ?', (customerID,)).fetchall()
 
         connection.close()
+
+        # Check if customer exists and is active
+        if customerActivity[0][0] == 0:
+            invalid_request_count[header_customer_id] += 1
+            return 'Customer inactive!'
 
 
         # Check if IP correct
@@ -160,13 +175,14 @@ def index():
 @app.route('/stats', methods = ['GET'])
 def stats():
 
-    # Get date parameters from request
+    # Get parameters from request
     day = request.args.get('day')
     month = request.args.get('month')
     year = request.args.get('year')
 
     customer_id = request.args.get('customer_id')
 
+    # For specific day statistics
     if day and month and year:
 
         try:
@@ -184,35 +200,58 @@ def stats():
         # Convert to timestamps
         day_start_timestamp = get_day_start.timestamp()
         day_end_timestamp = get_day_end.timestamp()
+        
+        try: 
+            # Connect to database
+            connection = sqlite3.connect('database.db')
+            cur = connection.cursor()
 
-        # Sum requests between start and end timestamps from database
-        connection = sqlite3.connect('database.db')
-        cur = connection.cursor()
-
-        total_requests_in_day = cur.execute('SELECT SUM(request_count) FROM hourly_stats WHERE time BETWEEN ? AND ?', (day_start_timestamp, day_end_timestamp)).fetchall()
-
-        connection.close()
-
-        if customer_id:
-
-            try:
-                connection = sqlite3.connect('database.db')
-                cur = connection.cursor()
+            # If customer id provided
+            if customer_id:
 
                 requests_per_customer = cur.execute('SELECT SUM(request_count), SUM(invalid_count) FROM hourly_stats WHERE (time BETWEEN ? AND ?) AND customer_id = ?', (day_start_timestamp, day_end_timestamp, customer_id)).fetchall()
 
-                connection.close()
-
-                if requests_per_customer[0][0] != None:
-                    return "Total requests on " + str(get_day_start.date()) + " for customer with id " + str(customer_id) + ": Total requests: " + str(requests_per_customer[0][0]) + ", Invalid requests: " + str(requests_per_customer[0][1])
-                else: 
+                if requests_per_customer[0][0] == None:
+                    connection.close()
                     return "Wrong customer ID!"
-            except:
-                return "Failed to fetch results from database!"
-            
+                else:
+                    connection.close()
+                    return "Total requests on " + str(get_day_start.date()) + " for customer with id " + str(customer_id) + ": Total requests: " + str(requests_per_customer[0][0]) + ", Invalid requests: " + str(requests_per_customer[0][1])
+
+            # If no customer_id provided, get all results for day
+            requests_in_day_per_customer = cur.execute('SELECT customer_id, SUM(request_count), SUM(invalid_count) FROM hourly_stats WHERE time BETWEEN ? AND ? GROUP BY customer_id ORDER BY customer_id DESC', (day_start_timestamp, day_end_timestamp)).fetchall()
+      
+            requests_in_day_total = cur.execute('SELECT SUM(request_count), SUM(invalid_count) FROM hourly_stats WHERE time BETWEEN ? AND ?', (day_start_timestamp, day_end_timestamp)).fetchall()
+       
+            connection.close()
 
 
-        return "Total number of requests on " + str(get_day_start.date()) + " is: " + str(total_requests_in_day[0][0] if total_requests_in_day[0][0]!= None else 0)
+            if requests_in_day_per_customer[0][0] == None:
+                return "No results for this date!"    
+            else: 
+                # Create a dictionary with stats per customer
+                result = {}
+
+                for row in requests_in_day_per_customer:
+
+                    customer = "customer_" + str(row[0])
+                    result[customer] = {
+                        'total_requests' : row[1],
+                        'invalid_requests' : row[2]
+                    }
+                
+                # Add the result for the total number per day 
+                    result['total_number'] = {
+                        'total_requests' : requests_in_day_total[0][0],
+                        'invalid_requests' : requests_in_day_total[0][1]
+                    }
+
+                json_result = jsonify(result)
+    
+                return json_result
+                
+        except:
+            return "Failed to fetch results from database!"
 
     else:   
         # If any of arguments not provided or arguments not numeric
@@ -236,7 +275,6 @@ def customerStats():
 
             stats_per_customer = cur.execute('SELECT time, SUM(request_count), SUM(invalid_count) FROM hourly_stats WHERE customer_id = ? GROUP BY time ORDER BY time DESC', (customer_id)).fetchall()
 
-            print(stats_per_customer)
             connection.close()
 
             result = {}
